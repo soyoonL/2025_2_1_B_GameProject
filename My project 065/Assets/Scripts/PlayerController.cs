29216cfb4060e59d6dd41435890aff27302811ba
soyoonL <diamomo777@naver.com>
2025-09-24 11:20:14 +0900

    [3강] 점프 및 공격

---
+++ My project 065/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,185 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("이동설정")]
+    public float walkSpeed = 3.0f;
+    public float runSpeed = 6.0f;
+    public float rotationSpeed = 10.0f;
+
+    [Header("점프설정")]
+    public float jumpHeight = 2.0f;
+    public float gravity = -9.81f;
+    public float landingDuration = 3.0f;
+
+    [Header("공격설정")]
+    public float attackDuration = 0.8f;
+    public bool canMoveWhileAttacking = false;
+
+    [Header("커포넌트")]
+    public Animator animator;
+
+    private CharacterController controller;
+    private Camera playerCamera;
+
+    private float currentSpeed;
+    private bool isAttacking = false;
+    private bool isLanding = false;
+    private float landingTimer;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+    private bool wasGrounded;
+    private float attackTimer;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerCamera = Camera.main;
+    }
+
+    
+    void Update()
+    {
+        CheckGrounded();
+        HandleLanding();
+        HandleMovement();
+        UpdateAnimator();
+        HandleAttack();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+
+        if((isAttacking && !canMoveWhileAttacking) || isLanding)
+        {
+            currentSpeed = 0;
+            return;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h != 0 || v != 0)
+        {
+            Vector3 cameraForward = playerCamera.transform.forward;
+            Vector3 cameraRight = playerCamera.transform.right;
+            cameraForward.y = 0;
+            cameraRight.y = 0;
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            Vector3 moveDirection = cameraForward * v + cameraRight * h;
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                currentSpeed = runSpeed;
+            }
+            else
+            {
+                currentSpeed = walkSpeed;
+            }
+
+           controller.Move(moveDirection*currentSpeed*Time.deltaTime);
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation,targetRotation,rotationSpeed*Time.deltaTime);
+        }
+        else
+        {
+            currentSpeed = 0;
+        }
+    }
+
+    void UpdateAnimator()
+    {
+        float animatorSpeed = Mathf.Clamp01(currentSpeed / runSpeed);
+        animator.SetFloat("speed",animatorSpeed);
+        animator.SetBool("isGrounded",isGrounded);
+
+        bool isFalling = !isGrounded && velocity.y < -0.1f;
+        animator.SetBool("isFalling", isFalling);
+        animator.SetBool("isLanding",isLanding);
+    }
+
+    void CheckGrounded()
+    {
+        wasGrounded = isGrounded;
+        isGrounded = controller.isGrounded;   
+
+        if (!isGrounded && wasGrounded)
+        {
+            Debug.Log("떨어지기 시작");      
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2.0f;
+
+            if (!wasGrounded && animator != null)  
+            {
+                isLanding = true;
+                landingTimer = landingDuration;
+            }
+        }
+    }
+
+    void HandleJump()
+    {
+        if(Input.GetButtonDown("Jump")&& isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            if(animator!=null)
+            {
+                animator.SetTrigger("jumpTrigger");
+            }
+        }
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        controller.Move(velocity*Time.deltaTime);
+    }
+
+    void HandleLanding()
+    {
+        if (isLanding)
+        {
+            landingTimer -= Time.deltaTime;
+            
+            if(landingTimer <= 0)
+            {
+                isLanding= false;
+            }
+        }
+    }
+
+    void HandleAttack()
+    {
+        if (isAttacking)
+        {
+            attackTimer -= Time.deltaTime;
+
+            if (attackTimer <= 0)
+            {
+                isAttacking = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && !isAttacking)
+        {
+            isAttacking = true;
+            attackTimer = attackDuration;
+
+            if (animator != null)
+            {
+                animator.SetTrigger("attackTrigger");
+            }
+        }
+    }
+
+
+}
