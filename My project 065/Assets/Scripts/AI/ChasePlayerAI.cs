1f919daeff7788ba3148b3c810d8e763114773e2
soyoonL <diamomo777@naver.com>
2025-11-20 13:57:36 +0900

    [12주차] NavMesh (길찾기) 완료

--- My project 065/Assets/Scripts/AI/ChasePlayerAI.cs
+++ My project 065/Assets/Scripts/AI/ChasePlayerAI.cs
@@ -5,30 +5,63 @@ using UnityEngine.AI;
 
 public class ChasePlayerAI : MonoBehaviour
 {
-    public Transform player;                
-    public float chaseRange = 50.01f;
+    public Transform player;
+    public float chaseRange = 50.0f;
     public float attackRange = 2.01f;
 
-    private NavMeshAgent agent;             
-    private float distanceToPlayer;         
+    private NavMeshAgent agent;
+    private float distanceToPlayer;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
-  
-void ChasePlayer()
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if(distanceToPlayer <= chaseRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasing();  
+        }
+
+        if(distanceToPlayer <= attackRange)
+        {
+            Attack();
+        }
+    }
+
+    void ChasePlayer()
     {
         agent.isStopped = false;
         agent.SetDestination(player.position);
     }
 
-    
-void Attack()
+    void Attack()
     {
         agent.isStopped = true;
         transform.LookAt(player);
         Debug.Log("Attacking player!");
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere (transform.position, attackRange);    
+    }
+
+    void StopChasing()
+    {
+        agent.isStopped = true;
+    }
+
+    
 }
